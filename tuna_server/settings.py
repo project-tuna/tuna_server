5363e8770b18ba7f2909a8e39fe8e1cfb329a2b2
raylin51 <raylzj51@gmail.com>
2019-05-05 09:14:46 +0000

    modify

--- tuna_server/settings.py
+++ tuna_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cs)ilnjkzy3slgh52i$0=5esq!0bifyjfk5nh@sdjlmou@37y%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
